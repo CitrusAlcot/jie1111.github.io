c7feaa82b927700987553aba5e534e03a01832e6
Jie He <jie1111@hotmail.com>
2018-07-20 13:13:27 -0400

    Signed-off-by: Jie He <jie1111@hotmail.com>

---
+++ Nanospread/game.js
@@ -0,0 +1,601 @@
+//This function is exclusively for avoiding loading issues when the level data has been changed
+//It will load whatever you've saved, and all subsequent level changes will use createGrid() instead
+//Even if that means a level reset causes a different level to appear.
+function createGridFromSave(savedGrid) {
+    theGrid = [];
+    for(var x = 0; x < 20; x++) {
+        theGrid[x] = [];
+    }
+    for (var column = 0; column < savedGrid.length; column++) {
+        for (var row = 0; row < savedGrid[column].length; row++) {
+            if(savedGrid[column][row]) {
+                theGrid[column][row] = new Square(column, row, 5);
+                for(var property in savedGrid[column][row]) {
+                    if (savedGrid[column][row].hasOwnProperty(property)) {
+                        if(property === "specialLevels") {
+                            continue;
+                        }
+                        theGrid[column][row][property] = savedGrid[column][row][property];
+                    }
+                }
+                theGrid[column][row].isSelected = 0;
+            }
+        }
+    }
+    if(!theView) {
+        theView = new View();
+    }
+}
+
+
+function createGrid() {
+    var startingCoords = {x:0,y:0};
+
+    theGrid = [];
+    for(var x = 0; x < 20; x++) {
+        theGrid[x] = [];
+    }
+    var wrapAroundLevel = currentLevel;
+    if(currentLevel >= levelData.length) {
+        wrapAroundLevel = (currentLevel-4) % (levelData.length-4) + 4; //first 4 levels are 'tutorial', and don't repeat
+    }
+    console.log(wrapAroundLevel);
+
+    var currentLevelGrid = levelData[wrapAroundLevel].grid;
+    var goalCost = Math.pow(2, currentLevel-2) * 100000000;
+    if(currentLevel === 0) {
+        goalCost /= 2000;
+    }
+    if(currentLevel === 1) {
+        goalCost /= 300;
+    }
+    if(currentLevel === 2) {
+        goalCost /= 250;
+    }
+    if(currentLevel === 3) {
+        goalCost /= 50;
+    }
+
+
+    var totalFromLevelData = 0;
+    for(var column = 0; column < currentLevelGrid[0].length; column++) {
+        for (var row = 0; row < currentLevelGrid.length; row++) {
+            var levelDataNum = currentLevelGrid[row][column];
+            if(levelDataNum === -1) {
+                startingCoords.x = column;
+                startingCoords.y = row;
+            }
+            if(levelDataNum > 0) {
+                totalFromLevelData += Math.pow(2, levelDataNum);
+            }
+        }
+    }
+
+    var consumeCostModifier = goalCost / totalFromLevelData;
+
+    var initialCostGrid = [];
+    for(x = 0; x < currentLevelGrid.length; x++) {
+        initialCostGrid[x] = [];
+        for(var y = 0; y < currentLevelGrid[x].length; y++) {
+            initialCostGrid[x][y] = (Math.pow(2, currentLevelGrid[x][y]) * consumeCostModifier) * 2;
+        }
+    }
+
+
+    for (column = 0; column < currentLevelGrid[0].length; column++) {
+        for (row = 0; row < currentLevelGrid.length; row++) {
+            if(currentLevelGrid[row][column] === 0) {
+                continue; //this makes the empty spots undefined in theGrid. Change if you want other terrain there.
+            }
+            var initialConsumeCost = initialCostGrid[row][column];
+            theGrid[column][row] =  new Square(column, row, initialConsumeCost);
+        }
+    }
+
+    doToAllSquares(function (square) {
+        square.chooseStartingDirection(); //All of them have a transfer target
+    }, false);
+
+    var startingSquare = theGrid[startingCoords.x][startingCoords.y];
+    startingSquare.gainNanites(startingSquare.consumeCost);
+}
+
+function tick() {
+    if(stop) {
+        return;
+    }
+    timer++;
+	tickGrowth();
+    handleFPSDifference();
+    clearNanitesReceived();
+    sendNanites();
+	tickStats();
+	if(settings.autobuyToggle === 1) {
+		autobuyLevels();
+	}
+	theView.update();
+
+    if(timer % 30 === 0) {
+        save();
+    }
+}
+
+function autobuyLevels() {
+    doToAllSquares(function (square) {
+        if((square.naniteAmount + settings.autobuyPerTick) <= autobuy.currentMax && (square.nanites * (autobuy.amtToSpend / 100)) >= square.naniteCostAfterMultiBuy(settings.autobuyPerTick)) {
+            square.autobuyMultipleNanites(settings.autobuyPerTick);
+        }
+    }, true);
+}
+
+function clearNanitesReceived() {
+    doToAllSquares(function (square) {
+        var target = theGrid[square.targetCol][square.targetRow];
+        target.naniteAmountReceived = 0;
+        target.advBotAmountReceived = 0;
+    }, false);
+}
+
+function sendNanites() {
+    doToAllSquares(function (square) {
+        var target = theGrid[square.targetCol][square.targetRow];
+        if(target.isActive()) {
+            target.gainAdvBots(square.sendPieceOfAdvBots()); //transfer .1% advBots
+        }
+        var amountTransferred = square.sendPieceOfNanites();
+        target.gainNanites(amountTransferred); //transfer .1% nanites
+        target.naniteAmountReceived += amountTransferred;
+		stats.transferredThisLevel += amountTransferred;
+		stats.totalTransferred += amountTransferred;
+
+        amountTransferred = square.sendPieceOfAdvBots();
+        target.gainAdvBots(amountTransferred); //transfer .1% adv bots
+        target.advBotAmountReceived += amountTransferred;
+
+        square.gainNanites(square.naniteRate * getNaniteGainBonus());
+        square.gainAdvBots(square.advBotRate);
+		stats.producedThisLevel += (square.naniteRate * getNaniteGainBonus());
+		stats.totalProduced += (square.naniteRate * getNaniteGainBonus());
+    }, true);
+}
+
+function getNaniteGainBonus() {
+    return ((bonuses.points * 5)+100)/100 * bonuses.growthBonus;
+}
+
+function clickedSquare(col, row) {
+    var square = theGrid[col][row];
+    //multiple are selected, selectOne setting is on, clicked a selected
+    if(!settings.selectOneOrMultiple && selected.length > 1) {
+        theView.setSelectedFalse();
+        square.isSelected = true;
+        selected.push(square);
+        menuOpen = "";
+        theView.updateInfoBox();
+        adjustMenus();
+        return;
+    }
+
+    square.isSelected = !square.isSelected;
+    var pos = selected.indexOf(square);
+    if(pos === -1) { //click new square
+        menuOpen = "";
+        if(!settings.selectOneOrMultiple && selected.length > 0) { //single
+            theView.setSelectedFalse();
+            selected.push(square);
+        } else { //multiple
+            selected.push(square);
+        }
+        theView.updateInfoBox();
+    } else {
+        theView.changeBorderSelected(square);
+        selected.splice(pos, 1);
+        theView.updateInfoBox();
+    }
+    adjustMenus();
+    showOrHideBox();
+}
+
+function singleSelect(xDelta, yDelta) {
+    if(selected.length === 0) {
+        return;
+    }
+    var lastSelected = selected[selected.length - 1];
+    var targetX = lastSelected.col + xDelta;
+    var targetY = lastSelected.row + yDelta;
+    if(targetX < 0 || targetX > theGrid.length || targetY < 0 || !theGrid[targetX][targetY]) {
+        return;
+    }
+    clickedSquare(targetX, targetY);
+}
+
+function changeDirectionOfSelected(direction) {
+    if(selected.length === 0) {
+        return;
+    }
+    for(var i = 0; i < selected.length; i++) {
+        var tempDirection = selected[i].transferDirection;
+        selected[i].changeTargetDirection(direction);
+        if(!selected[i].getTarget()) {
+            selected[i].changeTargetDirection(tempDirection);
+        }
+    }
+    theView.updateInfoBox();
+    for(i = 0; i < selected.length; i++) {
+        theView.updateDirectionDot(selected[i]);
+    }
+}
+
+function buyAll() {
+    for(var i = 0; i < selected.length; i++) {
+        if(selected[i].canBuyNanitesAfterMultiBuy()) {
+            selected[i].buyMultipleNanites(settings.buyPerClick);
+        }
+    }
+}
+
+function buyNanitesButtonPressed() {
+    if(selected.length === 0) {
+        return;
+    }
+    if(settings.buyLowestOrAll === 1) {
+        buyAll();
+    } else if(settings.buyLowestOrAll === 0) {
+        buyLowest();
+    } else {
+        buyLowestEach();
+    }
+    theView.updateInfoBox();
+    for(var i = 0; i < selected.length; i++) {
+        theView.updateDisplayNum(selected[i], settings.selectedResourceNum === 0 ? 'nanite' : 'advBot');
+    }
+}
+function buyLowest() {
+    var lowestAmountSquares = select.getLowestSquares(select.getSelectedActive(), 'nanite');
+    var allBuyable = true;
+    for(var i = 0; i < lowestAmountSquares.length; i++) {
+        allBuyable = allBuyable && lowestAmountSquares[i].canBuyNanitesAfterMultiBuy();
+    }
+    if(allBuyable) {
+        for(i = 0; i < lowestAmountSquares.length; i++) {
+            lowestAmountSquares[i].buyMultipleNanites(settings.buyPerClick);
+        }
+    }
+}
+function buyLowestEach() {
+    var lowestAmountSquares = select.getLowestSquares(select.getSelectedActive(), 'nanite');
+    for(var i = 0; i < lowestAmountSquares.length; i++) {
+        if(lowestAmountSquares[i].isActive() && lowestAmountSquares[i].canBuyNanitesAfterMultiBuy()) {
+            lowestAmountSquares[i].buyMultipleNanites(settings.buyPerClick);
+        }
+    }
+}
+
+function buyAdvBotsButtonPressed() {
+    for(var i = 0; i < selected.length; i++) {
+        if(selected[i].canBuyAdvBots()) {
+            selected[i].buyAdvBots();
+        }
+    }
+    theView.updateInfoBox();
+}
+
+function handleFPSDifference() {
+    timeList.push(new Date().getTime());
+    if(timeList.length > 10) {
+        timeList.splice(0, 1);
+        var fps = msWaitTime/((timeList[timeList.length-1] - timeList[0]) / (timeList.length-1))*100;
+        multFromFps = 100/fps;
+        //$scope.fps = round(fps)+"%";
+    } else {
+        multFromFps = 1;
+        //$scope.fps = "...";
+    }
+}
+
+function changeLevel(newLevel) {
+    if(!document.getElementById('levelConfirm').checked || newLevel < 0 || newLevel > highestLevel) {
+        console.log('level change blocked');
+        return;
+    }
+    resetDerivBonuses();
+    document.getElementById('levelConfirm').checked = "";
+    theView.setSelectedFalse();
+    var EPGain = calcEvolutionPointGain();
+    bonuses.points += EPGain;
+    bonuses.availableEP += EPGain;
+	if(EPGain > 0) {
+		stats.totalLevels++;
+	}
+    if(document.getElementById('resetAvailableEP').checked) {
+        resetBonusPoints();
+    }
+    var keepDirections;
+    if(newLevel === currentLevel) {
+        keepDirections = theGrid;
+    }
+    currentLevel = newLevel;
+	statsUpdate();
+    createGrid();
+    theView.createGrid();
+    if(keepDirections) {
+        applyDirectionsToGrid(keepDirections);
+    }
+	setTransferRate(bonuses.transferRateLevel);
+    theView.update();
+}
+
+function applyDirectionsToGrid(tempGrid) {
+    for (var column = 0; column < theGrid.length; column++) {
+        for (var row = 0; row < theGrid[column].length; row++) {
+            var oldSquare = tempGrid[column][row];
+            var square = theGrid[column][row];
+            if(oldSquare && square) {
+                square.changeTargetDirection(oldSquare.transferDirection);
+                if(!square.getTarget()) {
+                    square.chooseStartingDirection();
+                }
+            }
+        }
+    }
+}
+
+function calcEvolutionPointGain() {
+    var bonus = 9999999999;
+    for (var column = 0; column < theGrid.length; column++) {
+        for (var row = 0; row < theGrid[column].length; row++) {
+            var square = theGrid[column][row];
+            if (square && square.curSpecialPosNanites < bonus) { //gets lowest
+                bonus = square.curSpecialPosNanites;
+            }
+        }
+    }
+    if(bonus > 0 && currentLevel === highestLevel) {
+        highestLevel++;
+        console.log('level up!');
+    }
+    return bonus === 0 ? 0 : ((currentLevel+1) * Math.pow(1.2, bonus) * (1 + (calcTotalAchieveBonus() / 100)));
+}
+
+function recalcInterval(newSpeed) {
+    bonuses.tickSpeedLevel = newSpeed;
+    doWork.postMessage({stop:true});
+    doWork.postMessage({start:true,ms:(1000 / newSpeed)});
+}
+
+function buyTickSpeed() {
+    if(bonuses.availableEP >= getTickSpeedCost() && bonuses.tickSpeedLevel < 5) {
+		bonuses.availableEP -= getTickSpeedCost();
+		recalcInterval(bonuses.tickSpeedLevel + .2);
+    }
+    theView.updateUpgrade();
+}
+
+function getTickSpeedCost(tickSpeedLevel) {
+    if(tickSpeedLevel === undefined) {
+        tickSpeedLevel = bonuses.tickSpeedLevel;
+    }
+    return precision2(50 * Math.pow(2, (tickSpeedLevel - 1)*5));
+}
+
+function setTransferRate(newRate) {
+    bonuses.transferRateLevel = newRate;
+    doToAllSquares(function (square) {
+        square.transferRate = newRate;
+    }, false);
+}
+
+function buyTransferRate() {
+    if(bonuses.availableEP >= getTransferRateCost() && bonuses.transferRateLevel < 10) {
+		bonuses.availableEP -= getTransferRateCost();
+		setTransferRate(bonuses.transferRateLevel + 1);
+    }
+    theView.update();
+}
+
+function getTransferRateCost(transferRateLevel) {
+    if(transferRateLevel === undefined) {
+        transferRateLevel = bonuses.transferRateLevel;
+    }
+    return precision2(15 * Math.pow(2, transferRateLevel - 1));
+}
+
+function buyDiscountLevel() {
+    if(bonuses.availableEP >= getDiscountCost()) {
+        bonuses.availableEP -= getDiscountCost();
+        bonuses.discountLevel++;
+        doToAllSquares(function (square) {
+            square.naniteCost = square.calcNaniteCost();
+        }, false);
+    }
+    theView.update();
+}
+
+function getCostReduction(discountLevel) {
+    return Math.pow(1.01, discountLevel)*5 - 4;
+}
+
+function getDiscountCost(discountLevel) {
+    if(discountLevel === undefined) {
+        discountLevel = bonuses.discountLevel;
+    }
+    return precision2(Math.pow(1.03, discountLevel) * Math.pow(discountLevel+1, 2)); // 1.01^n * (n+1)^2
+}
+
+function buyAbMaxLevel() {
+    if(autobuy.currentMax < highestLevel * 10 && bonuses.availableEP >= getAbMaxCost()) {
+		bonuses.availableEP -= getAbMaxCost();
+		autobuy.currentMax++;
+    }
+    theView.update();
+}
+
+function getAbMaxCost(currentMax) {
+    if(currentMax === undefined) {
+        currentMax = autobuy.currentMax;
+    }
+    return precision2(10 / 4 * currentMax);
+}
+
+function buyAbAmtToSpendLevel() {
+    if(autobuy.amtToSpend < 50 && bonuses.availableEP >= getAbAmtToSpendCost()) {
+		bonuses.availableEP -= getAbAmtToSpendCost();
+		autobuy.amtToSpend++;
+    }
+    theView.update();
+}
+
+function getAbAmtToSpendCost(amtToSpend) {
+    if(amtToSpend === undefined) {
+        amtToSpend = autobuy.amtToSpend;
+    }
+    return precision2(25 * amtToSpend);
+}
+
+function resetBonusPoints() {
+    resetEPUpgrades();
+    bonuses.availableEP = bonuses.points;
+    theView.update();
+}
+
+function doToAllSquares(functionToRun, onlyIsActive) {
+    for (var column = 0; column < theGrid.length; column++) {
+        for (var row = 0; row < theGrid[column].length; row++) {
+            var square = theGrid[column][row];
+            if(square && (!onlyIsActive || square.isActive())) {
+                functionToRun(square);
+            }
+        }
+    }
+}
+
+function statsUpdate() {
+	if(stats.ticksThisLevel > stats.highestTicks) {
+		stats.highestTicks = stats.ticksThisLevel;
+	}
+	if(stats.highestTicks >= nextAchieveLevelGoal(achieves.highestTicksAch)) {
+		while(stats.highestTicks >= nextAchieveLevelGoal(achieves.highestTicksAch)) {
+			achieves.highestTicksAch++;
+		}
+	}
+	if(stats.producedThisLevel > stats.highestProduced) {
+		stats.highestProduced = stats.producedThisLevel;
+	}
+	if(stats.highestProduced >= nextAchieveLevelGoal(achieves.highestProducedAch)) {
+		while(stats.highestProduced >= nextAchieveLevelGoal(achieves.highestProducedAch)) {
+			achieves.highestProducedAch++;
+		}
+	}
+	if(stats.transferredThisLevel > stats.highestTransferred) {
+		stats.highestTransferred = stats.transferredThisLevel;
+	}
+	if(stats.highestTransferred >= nextAchieveLevelGoal(achieves.highestTransferredAch)) {
+		while(stats.highestTransferred >= nextAchieveLevelGoal(achieves.highestTransferredAch)) {
+			achieves.highestTransferredAch++;
+		}
+	}
+	if(highestLevel >= ((achieves.highestLevelAch + 1) * 10)) {
+		while(highestLevel >= ((achieves.highestLevelAch + 1) * 10)) {
+			achieves.highestLevelAch++;
+		}
+	}
+	if(stats.totalLevels >= ((achieves.totalLevelsAch + 1) * 10)) {
+		while(stats.totalLevels >= ((achieves.totalLevelsAch + 1) * 10)) {
+			achieves.totalLevelsAch++;
+		}
+	}
+	stats.ticksThisLevel = 0;
+	stats.producedThisLevel = 0;
+	stats.transferredThisLevel = 0;
+}
+
+function tickStats() {
+	stats.ticksThisLevel++;
+	stats.totalTicks++;
+	if(stats.totalTicks >= nextAchieveLevelGoal(achieves.totalTicksAch)) {
+		while(stats.totalTicks >= nextAchieveLevelGoal(achieves.totalTicksAch)) {
+			achieves.totalTicksAch++;
+		}
+	}
+	if(stats.totalProduced >= nextAchieveLevelGoal(achieves.totalProducedAch)) {
+		while(stats.totalProduced >= nextAchieveLevelGoal(achieves.totalProducedAch)) {
+			achieves.totalProducedAch++;
+		}
+	}
+	if(stats.totalTransferred >= nextAchieveLevelGoal(achieves.totalTransferredAch)) {
+		while(stats.totalTransferred >= nextAchieveLevelGoal(achieves.totalTransferredAch)) {
+			achieves.totalTransferredAch++;
+		}
+	}
+}
+function nextAchieveLevelGoal(achievement) {
+	return 10 * Math.pow(10, achievement);
+}
+
+function calcAchieveBonus(achievement) {
+    return achievement * ((0.1 + (achievement * 0.1)) / 2);
+}
+
+function calcTotalAchieveBonus() {
+	var totalAchieveBonus = 0;
+	totalAchieveBonus += calcAchieveBonus(achieves.highestTicksAch);
+	totalAchieveBonus += calcAchieveBonus(achieves.totalTicksAch);
+	totalAchieveBonus += calcAchieveBonus(achieves.highestProducedAch);
+	totalAchieveBonus += calcAchieveBonus(achieves.totalProducedAch);
+	totalAchieveBonus += calcAchieveBonus(achieves.highestTransferredAch);
+	totalAchieveBonus += calcAchieveBonus(achieves.totalTransferredAch);
+	totalAchieveBonus += calcAchieveBonus(achieves.highestLevelAch);
+	totalAchieveBonus += calcAchieveBonus(achieves.totalLevelsAch);
+	return round2(totalAchieveBonus);
+}
+
+function tickGrowth() {
+    for(var x = bonuses.derivs.length - 1; x >= 0; x--) {
+        var deriv = bonuses.derivs[x];
+        deriv.currentTicks++;
+        if(deriv.currentTicks >= deriv.ticksNeeded) {
+            deriv.currentTicks -= deriv.ticksNeeded;
+            if(x === 0) {
+                //deriv.amount  deriv.gainMultiplier
+                bonuses.growthBonus += (deriv.amount * Math.pow(2, deriv.upgradeAmount)) / 10000;
+                continue;
+            }
+            var prevDeriv = bonuses.derivs[x-1];
+            prevDeriv.amount += deriv.amount * Math.pow(2, deriv.upgradeAmount);
+        }
+    }
+}
+
+function resetDerivBonuses() {
+    bonuses.growthBonus = 1;
+    for(var x = 0; x < bonuses.derivs.length; x++) {
+        bonuses.derivs[x].ticksNeeded = x === 0 ? 4 : Math.pow(2, (x-1))*10;
+        bonuses.derivs[x].currentTicks = 0;
+        bonuses.derivs[x].upgradeAmount = 0;
+        bonuses.derivs[x].amount = 1;
+    }
+}
+
+function calcCostForNextDeriv() {
+    return precision2(factorial(bonuses.derivs.length+4)*4.2); //Starts at 100
+}
+
+function buyNextDeriv() {
+    var cost = calcCostForNextDeriv();
+    if(bonuses.availableEP >= cost) {
+        bonuses.availableEP -= cost;
+        bonuses.points -= cost;
+        addDeriv();
+    }
+}
+
+function addDeriv() {
+    var pos = bonuses.derivs.length;
+    bonuses.derivs[pos] = {};
+    var newDeriv = bonuses.derivs[pos];
+    newDeriv.ticksNeeded = pos === 0 ? 4 : Math.pow(2, (pos-1))*10;
+    newDeriv.currentTicks = 0;
+    newDeriv.upgradeAmount = 0;
+    newDeriv.amount = 1;
+    theView.recreateDerivs();
+}
---
+++ Nanospread/saving.js
@@ -0,0 +1,171 @@
+var doWork = new Worker('interval.js');
+doWork.onmessage = function (event) {
+    if (event.data === 'interval.start') {
+        tick();
+    }
+};
+
+var settings = {};
+var theView;
+var selected = [];
+var timeList = [];
+var timer = 0;
+var stop = 0;
+var multFromFps = 1;
+var msWaitTime = 1000;
+var theGrid = [];
+var menuOpen = "";
+var upgradeTab;
+var startingDragPoint = {};
+var endingDragPoint = {};
+var select = new Select();
+var isDragging = false;
+var rclickStartingPoint;
+var currentLevel, highestLevel;
+var bonuses, autobuy, stats, achieves;
+var specialLevels = [0, 10, 25, 50, 75, 100, 125, 150, 175, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 775, 850, 925, 1000, 1075, 1150, 1225, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2200, 2400, 2600, 2800, 3000, 3300, 3600, 3900, 4200, 4500, 4800, 5200, 5600, 6000, 6600, 7300, 7900, 8500, 9400, 10000];
+
+function getSpecialLevels(pos) {
+    if(specialLevels[pos] === undefined) {
+        specialLevels[pos] = 10000 + (pos+1 - specialLevels.length);
+    }
+    return specialLevels[pos];
+}
+
+function clearSave() {
+    window.localStorage.version4 = "";
+    load();
+}
+
+function loadDefaults() {
+    settings.buyPerClick = 1;
+	settings.autobuyToggle = 0;
+	settings.autobuyPerTick = 1;
+    settings.selectedResourceNum = 0;
+    settings.selectGridFont = 0;
+
+    settings.selectOneOrMultiple = 0;
+    settings.buyLowestOrAll = 1;
+    settings.showLastOrLowest = 0;
+    settings.selectAllOrLowestBorderColor = 0;
+    settings.selectShowNoneOrNanitesOrAmount = 3;
+    highestLevel = 0; //SHOULD BE 0 BEFORE COMMIT
+    currentLevel = 0;
+
+    bonuses = { points:0, availableEP:0};
+    autobuy = {};
+    resetEPUpgrades();
+    bonuses.derivs = [];
+    resetDerivBonuses();
+	stats = { ticksThisLevel:0, totalTicks:0, producedThisLevel:0, totalProduced:0, transferredThisLevel:0, totalTransferred:0, totalLevels:0, highestTicks:0, highestProduced:0, highestTransferred:0};
+	achieves = { highestTicksAch:0, totalTicksAch:0, highestProducedAch:0, totalProducedAch:0, highestTransferredAch:0, totalTransferredAch:0, highestLevelAch:0, totalLevelsAch:0};
+    openUpgradeTab();
+}
+
+function resetEPUpgrades() {
+    bonuses.tickSpeedLevel = 1;
+    bonuses.transferRateLevel = 1;
+    bonuses.discountLevel = 0;
+    autobuy.currentMax = 1;
+    autobuy.amtToSpend = 1;
+}
+
+function toggleIMessedUpPopup1() { //Let's hope there's not more
+    if(document.getElementById("imessedup").style.display === "block") {
+        document.getElementById("imessedup").style.display = "none";
+    } else {
+        document.getElementById("imessedup").style.display = "block";
+    }
+}
+
+function clearBoard() {
+    createGrid();
+    if(!theView) {
+        theView = new View();
+    }
+    theView.createGrid();
+    theView.update();
+    recalcInterval(bonuses.tickSpeedLevel);
+}
+
+function load() {
+    loadDefaults();
+    if (!window.localStorage.version4) { //New players to the game
+        clearBoard();
+        if(window.localStorage.version3) { //New players to version 4
+            var oldLoad = JSON.parse(window.localStorage.version3);
+            var roughBonus = ((oldLoad.bonuses.tickSpeedLevel - 1)  * 5) + oldLoad.bonuses.transferRateLevel * .4 + oldLoad.bonuses.discountLevel * .2 + oldLoad.autobuy.currentMax * .3 + oldLoad.autobuy.amtToSpend * .5;
+            roughBonus = round2(Math.pow(2, roughBonus));
+            oldLoad.bonuses.points += roughBonus;
+            bonuses.points = oldLoad.bonuses.points > 1000 ? 1000 : oldLoad.bonuses.points;
+            toggleIMessedUpPopup1();
+            document.getElementById("messedupEP").innerHTML = bonuses.points;
+            window.localStorage.version3 = "";
+        }
+        openHelpBox();
+        return;
+    }
+    var toLoad = JSON.parse(window.localStorage.version4);
+    currentLevel = toLoad.currentLevel;
+
+    //Handles a change in properties
+    for(var property in toLoad.bonuses) {
+        if (toLoad.bonuses.hasOwnProperty(property)) {
+            bonuses[property] = toLoad.bonuses[property];
+        }
+    }
+
+    for(property in toLoad.autobuy) {
+        if (toLoad.autobuy.hasOwnProperty(property)) {
+            autobuy[property] = toLoad.autobuy[property];
+        }
+    }
+	for(property in toLoad.stats) {
+        if (toLoad.stats.hasOwnProperty(property)) {
+            stats[property] = toLoad.stats[property];
+        }
+    }
+	for(property in toLoad.achieves) {
+        if (toLoad.achieves.hasOwnProperty(property)) {
+            achieves[property] = toLoad.achieves[property];
+        }
+    }
+
+    settings = toLoad.settings;
+    highestLevel = toLoad.highestLevel;
+    settings.buyPerClick = 1; //resets on refresh
+    settings.selectedResourceNum = 0;
+
+    createGridFromSave(toLoad.theGrid);
+    setTransferRate(bonuses.transferRateLevel);
+    recalcInterval(bonuses.tickSpeedLevel);
+}
+
+function save() {
+    var toSave = {};
+    toSave.settings = settings;
+    toSave.currentLevel = currentLevel;
+    toSave.theGrid = theGrid;
+    toSave.highestLevel = highestLevel;
+    toSave.bonuses = bonuses;
+	toSave.autobuy = autobuy;
+	toSave.stats = stats;
+	toSave.achieves = achieves;
+    // console.log('saved');
+    window.localStorage.version4 = JSON.stringify(toSave);
+}
+
+function exportSave() {
+    save();
+    document.getElementById("exportImport").value = encode(window.localStorage.version4);
+    document.getElementById("exportImport").select();
+    document.execCommand('copy');
+    document.getElementById("exportImport").value = "";
+}
+
+function importSave() {
+    window.localStorage.version4 = decode(document.getElementById("exportImport").value);
+    load();
+}
+
+load();
